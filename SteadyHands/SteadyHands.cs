6238436f07b4bc18c29a803aa5b0bbf6a6cf082d
AuthorX <40285046+AuthorX@users.noreply.github.com>
2023-09-21 17:48:58 -0500

    Add menu options

--- SteadyHands/SteadyHands.cs
+++ SteadyHands/SteadyHands.cs
@@ -8,22 +8,22 @@ using UnityEngine.Assertions;
 
 namespace SteadyHands
 {
-    public class LocalSettingsClass
+    public class GlobalSettingsClass
     {
         public bool needsQuill = true;
         public bool isEnabled = true;
     }
 
-    public class SteadyHandsMod : Mod, ILocalSettings<LocalSettingsClass>
+    public class SteadyHandsMod : Mod, IGlobalSettings<GlobalSettingsClass>, IMenuMod, ITogglableMod
     {
         private static SteadyHandsMod? _instance;
         private static GameManager? gm;
         private static GameMap? map;
         private static GameObject? msg;
 
-        public LocalSettingsClass saveSettings { get; set; } = new LocalSettingsClass();
-        public void OnLoadLocal(LocalSettingsClass s) => this.saveSettings = s;
-        public LocalSettingsClass OnSaveLocal() => this.saveSettings;
+        public GlobalSettingsClass saveSettings { get; set; } = new GlobalSettingsClass();
+        public void OnLoadGlobal(GlobalSettingsClass s) => this.saveSettings = s;
+        public GlobalSettingsClass OnSaveGlobal() => this.saveSettings;
 
 
         internal static SteadyHandsMod Instance
@@ -37,6 +37,8 @@ namespace SteadyHands
                 return _instance;
             }
         }
+
+        public bool ToggleButtonInsideMenu { get; } = true;
 
         public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
 
@@ -82,6 +84,31 @@ namespace SteadyHands
             //    GameObject go = GameObject.Instantiate(msg);
             //    go.SetActive(true);
             //}
+        }
+
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            return new List<IMenuMod.MenuEntry>
+            {
+                toggleButtonEntry.GetValueOrDefault(),
+                new IMenuMod.MenuEntry {
+                    Name = "Require Quill?",
+                    // Description = "Automatically adds new areas to the map",
+                    Values = new string[] {
+                        "On",
+                        "Off"
+                    },
+                    Saver = opt => saveSettings.needsQuill = opt == 0,
+                    Loader = () => saveSettings.needsQuill ? 0 : 1
+                }
+            };
+        }
+
+        public void Unload()
+        {
+            On.GameMap.Start -= GameMap_Start;
+            On.SceneManager.AddSceneMapped -= SceneManager_AddSceneMapped;
+            ModHooks.GetPlayerBoolHook -= CheckQuill;
         }
 
         //public override (string, Func<IEnumerator>)[] PreloadSceneHooks()
